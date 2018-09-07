db4466d000a6fc20fee2c9dce52ec2e30f261ab2
Nick Holt <naholt01@gmail.com>
2018-09-07 11:28:19 -0400

    Initial commit

---
+++ useful functions.R
@@ -0,0 +1,8 @@
+# function that takes a data frame object and standardizes the column names
+library(stringr)
+
+name_clean <- function(df) {
+        colnames(df) <- str_remove_all(tolower(colnames(df)), "[[:punct:]]")
+        colnames(df) <- str_replace_all(tolower(colnames(df)), " ", "_")
+        return(df)
+}

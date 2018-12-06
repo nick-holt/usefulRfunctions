8ca6777655ff486a80c9fe626739bd68e191edcc
Nick Holt <naholt01@gmail.com>
2018-12-05 22:53:14 -0500

    added crossTilePlot function

--- useful functions.R
+++ useful functions.R
@@ -9,3 +9,25 @@ name_clean <- function(df) {
 
 # function that is opposite of %in%
 '%!in%' <- function(x,y)!('%in%'(x,y))
+
+# function that produces a heatmap tile plot for two factors (likert survey data)
+crossTilePlot <- function(x, y) {
+        data <- data.frame(x, y) %>%
+                filter(!is.na(y)) %>%
+                filter(!is.na(x))
+        combos <- data %>% expand(x, y)
+        missing <- anti_join(combos, data) %>%
+                mutate(n = 0)
+        data %>%
+                group_by(x, y) %>%
+                summarize(n = n()) %>%
+                bind_rows(missing) %>%
+                ggplot(aes(x, y, fill = n)) +
+                        geom_tile() +
+                        geom_text(aes(label = n)) +
+                        scale_fill_gradient(low = "white", high = "lightblue") +
+                        xlab(paste0("\n", name_x)) +
+                        ylab(paste0(name_y, "\n")) +
+                theme(axis.title = element_text(face="bold", size=11)) 
+        
+}
